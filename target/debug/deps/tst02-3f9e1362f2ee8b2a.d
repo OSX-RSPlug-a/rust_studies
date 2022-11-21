80e6d01f9f75960167df82ebacbe6825e0d0f10e
Fernando <Log-N@outlook.com>
2022-11-21 15:01:12 -0300

    Add files via upload

---
+++ target/debug/deps/tst02-3f9e1362f2ee8b2a.d
@@ -0,0 +1,5 @@
+/home/kagura/Projects/rust_test/tst02/target/debug/deps/tst02-3f9e1362f2ee8b2a: src/main.rs
+
+/home/kagura/Projects/rust_test/tst02/target/debug/deps/tst02-3f9e1362f2ee8b2a.d: src/main.rs
+
+src/main.rs:
