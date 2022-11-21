bd02cc60853bf5341139f9fa58d0dda14b1b43e9
Fernando <Log-N@outlook.com>
2022-11-21 15:02:14 -0300

    Delete target directory

--- target/debug/deps/tst02-3f9e1362f2ee8b2a.d
+++
@@ -1,5 +0,0 @@
-/home/kagura/Projects/rust_test/tst02/target/debug/deps/tst02-3f9e1362f2ee8b2a: src/main.rs
-
-/home/kagura/Projects/rust_test/tst02/target/debug/deps/tst02-3f9e1362f2ee8b2a.d: src/main.rs
-
-src/main.rs:
--- target/debug/tst02.d
+++
@@ -1,1 +0,0 @@
-/home/kagura/Projects/rust_test/tst02/target/debug/tst02: /home/kagura/Projects/rust_test/tst02/src/main.rs
