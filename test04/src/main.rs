45865daa637dc6b4daf1949f461c8254595af032
Fernando <Log-N@outlook.com>
2022-12-01 01:13:14 -0300

    Add files via upload

---
+++ test04/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+
+fn main() {
+
+    // String types
+    let mut name= String::new();
+    name.push_str("Dude ");
+    name.push_str("Bug ");
+    name.push_str("Slug");
+    println!("{:?}", name);
+
+    let n= String::from("Wed");
+    println!("{n}");
+
+    let list = ['w', 'e', 'd', 'n', 'e', 's', 'd', 'a', 'y'];
+    let conc = String::from_iter(list);
+    println!("{conc}");
+
+    let s: String = "Wolfmother".into();
+    println!("{s}");
+
+    let x = "Shaman".to_owned();
+    println!("{x}");
+
+    let mut text = String::new();
+    println!("Insert a text");
+    
+    io::stdin()
+        .read_line(&mut text)
+        .expect("Error reading console");
+
+    println!("U`ve inserted {text}");
+
+}
